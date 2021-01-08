7f2f78ef58e8124279b1366f90e359565592e181
Colin Malcolm <colinmalcolm@Colins-Air.fios-router.home>
2021-01-08 00:06:54 -0500

    Signup, Signin, Checkout components wireframe created

---
+++ client/src/components/Checkout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Checkout extends React.Component {
+
+  render() {
+    return (
+      <div>Checkout Page</div>
+    )
+  }
+}
+
+
+
+
+export default Checkout
---
+++ client/src/components/Signin.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Signin extends React.Component {
+  render() {
+    return <div>Signin Page</div>;
+  }
+}
+
+export default Signin;
