2a438821a4071152125076040441422e8af75586
Colin Malcolm <colinmalcolm@Colins-Air.fios-router.home>
2021-01-08 00:23:52 -0500

    Routes created

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './index.css';
 import App from './components/App';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+import Checkout from './components/Checkout';
 import registerServiceWorker from './registerServiceWorker';
 
 
@@ -11,7 +14,7 @@ const Root = () => (
 
   <Router>
     <Switch>
-      <Route component={App} path="/" />
+      <Route component={App} exact path="/" />
       <Route component={Signin} path="/signin" />
       <Route component={Signup} path="/signup" />
       <Route component={Checkout} path="/checkout" />
