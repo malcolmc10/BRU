be6a2c32906f109d86362bb490676454a872a60a
Colin Malcolm <colinmalcolm@Colins-Air.fios-router.home>
2021-01-08 21:54:25 -0500

    added item and logos to the Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Text } from 'gestalt';
+import { Box, Text, Heading, Image } from 'gestalt';
 import { NavLink } from 'react-router-dom';
 import "../components/Navbar.css"
 
@@ -9,11 +9,49 @@ const Navbar = () => (
    color="purple"
     padding={1}
     shape="roundedBottom"
+    display="flex"
+    alignItems="center"
+    justifyContent="around"
   >
 
     {/* Signin Link goes here */}
     <NavLink to="/signin">
-      <Text>Sign In</Text>
+      <Text size="xl" color="lightGray">Sign In</Text>
+    </NavLink>
+
+    {/* Logo */}
+
+    <NavLink to="/">
+      <Box
+        display="flex"
+        alignItems="center"
+      >
+      <Box
+        height={50}
+          width={50}
+          margin={2}
+      >
+      <Image
+      alt="BRU-Logo"
+        src="./icons/beer.svg"
+        naturalHeight={1}
+        naturalWidth={1}
+        
+        />
+        </Box>
+      <Heading size="xs" color="white">
+
+        BRU
+
+      </Heading>
+      </Box>
+    </NavLink>
+
+    {/* Signup Link goes here */}
+    <NavLink to="/signup">
+      <Text size="xl" color="lightGray">
+        Sign Up
+      </Text>
 
     </NavLink>
 
